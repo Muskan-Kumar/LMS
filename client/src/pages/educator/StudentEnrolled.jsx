e1a0fb651594cc569b658cc695032671809ca272
Muskan Kumar <muskankumar952593@gmail.com>
2025-10-09 23:02:48 +0530

    Complete student page

---
+++ client/src/pages/educator/StudentEnrolled.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import { dummyStudentEnrolled } from '../../assets/assets'
+import loading from '../../components/student/Loading'
+
+const StudentEnrolled = () => {
+  const [enrolledStudents, setEnrolledStudents] = useState(null)
+
+  const fetchEnrolledStudents=async()=>{
+    setEnrolledStudents(dummyStudentEnrolled)
+  }
+
+  useEffect(()=>{
+    fetchEnrolledStudents()
+  },[])
+
+  return enrolledStudents ? (
+    <div className='min-h-screen flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0'>
+        <div className='flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20'>
+        <table className='table-fixed md:table-auto w-full overflow-hidden pb-4'>
+          <thead className='text-gray-900 border-b borger-gray-500/20 text-sm text-left'>
+          <tr>
+            <th className='px-4 py-3 font-semibold'>Student Name</th>
+            <th className='px-4 py-3 font-semibold'>Course Title</th>
+            <th className='px-4 py-3 font-semibold hiddeb sm:table-cell'>Date</th>
+          </tr>
+          </thead>
+        </table>
+        </div>
+
+    </div>
+  ): <loading />
+}
+
+export default StudentEnrolled
