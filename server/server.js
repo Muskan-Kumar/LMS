667999ab596d1ee18334226c5effd6fb1ca8435d
Muskan Kumar <muskankumar952593@gmail.com>
2025-10-11 17:55:29 +0530

    mongodb and webhook error fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import connectDB from './configs/mongodb.js';
+import {connectDB} from './configs/mongodb.js';
+import { clerkWebhook } from './controllers/webhooks.js';
 
 // Port
 const PORT = process.env.PORT || 5000;
@@ -9,11 +10,12 @@ const PORT = process.env.PORT || 5000;
 // initialize express
 const app = express();
 
-//connect to database
-await connectDB();
-
 // middleware
 app.use(cors());
+dotenv.config()
+
+//connect to database
+await connectDB();
 
 // Route
 app.get('/',(req,res)=>{
