907b4b98795e8f4b1d70a8d42764b330d87917e6
Muskan Kumar <muskankumar952593@gmail.com>
2025-10-17 23:26:18 +0530

    Backend completed

---
+++ server/models/CourseProgress.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const courseProgressSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    courseId: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: boolean,
+        default: false
+    },
+    lectureCompleted: [],
+}, {minimize: false})
+
+export const CourseProgress = mongoose.model('CourseProgress', courseProgressSchema)
+
